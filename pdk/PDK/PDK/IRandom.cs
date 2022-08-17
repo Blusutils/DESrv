7ca152ed0ae26765d804bac005fe8e9789ef5731
Егор Брон <bataikinegor@yandex.ru>
2022-08-17 10:48:19 +0300

    Started rewrition of connections and minor changes

---
+++ pdk/PDK/PDK/IRandom.cs
@@ -0,0 +1,7 @@
+﻿namespace DESPDK.Random {
+    public interface IRandom {
+        public void Seed(int value);
+        public int GetRandInt();
+        public int GetRandInt(int startRange, int endRange);
+    }
+}
