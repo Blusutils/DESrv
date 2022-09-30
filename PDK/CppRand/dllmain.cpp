cfa518ffc716daf098d77887e7c8ba0a807746c4
Егор Брон <71507444+EgorBron@users.noreply.github.com>
2022-09-30 21:42:50 +0300

    (final) Fix of CppRandom

--- PDK/CppRand/dllmain.cpp
+++ PDK/CppRand/dllmain.cpp
@@ -1,15 +1,16 @@
-﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
-#include "windows.h"
+﻿#include "windows.h"
 #include <random>
 
-__declspec(dllexport) void SeedDll(int value) {
-    std::srand(value);
-}
-__declspec(dllexport) int GetRandIntDll() {
-    return std::rand();
-}
-__declspec(dllexport) int GetRandIntDllRanged(int startRange, int endRange) {
-    return startRange + (std::rand() % static_cast<int>(endRange - startRange + 1));
+extern "C" {
+    __declspec(dllexport) void SeedDll(int value) {
+        std::srand(value);
+    }
+    __declspec(dllexport) int GetRandIntDll() {
+        return std::rand();
+    }
+    __declspec(dllexport) int GetRandIntDllRanged(int startRange, int endRange) {
+        return startRange + (std::rand() % static_cast<int>(endRange - startRange + 1));
+    }
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
