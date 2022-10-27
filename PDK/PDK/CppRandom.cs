3494456de27e5627d691e3042b87aafb0bc6813a
Егор Брон <71507444+EgorBron@users.noreply.github.com>
2022-10-27 14:42:09 +0300

    again - Remove CppRand from solution

--- PDK/PDK/CppRandom.cs
+++ PDK/PDK/CppRandom.cs
@@ -1,25 +1,25 @@
-﻿using System.Runtime.InteropServices;
+﻿//using System.Runtime.InteropServices;
 
-namespace DESPDK.Random {
-    [ComVisible(true)]
-    public class CppRandom : RandomBase {
-        [DllImport("CppRand.dll")]
-        public static extern int GetRandIntDll();
-        [DllImport("CppRand.dll")]
-        public static extern int GetRandIntDllRanged(int startRange, int endRange);
-        [DllImport("CppRand.dll")]
-        public static extern void SeedDll(int value);
+//namespace DESPDK.Random {
+//    [ComVisible(true)]
+//    public class CppRandom : RandomBase {
+//        [DllImport("CppRand.dll")]
+//        public static extern int GetRandIntDll();
+//        [DllImport("CppRand.dll")]
+//        public static extern int GetRandIntDllRanged(int startRange, int endRange);
+//        [DllImport("CppRand.dll")]
+//        public static extern void SeedDll(int value);
 
-        public override int GetRandInt() {
-            return GetRandIntDll();
-        }
+//        public override int GetRandInt() {
+//            return GetRandIntDll();
+//        }
 
-        public override int GetRandInt(int startRange, int endRange) {
-            return GetRandIntDllRanged(startRange, endRange);
-        }
+//        public override int GetRandInt(int startRange, int endRange) {
+//            return GetRandIntDllRanged(startRange, endRange);
+//        }
 
-        public override void Seed(int value) {
-            SeedDll(value);
-        }
-    }
-}
+//        public override void Seed(int value) {
+//            SeedDll(value);
+//        }
+//    }
+//}
