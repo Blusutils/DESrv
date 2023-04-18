85da5795599a8e8662f0e12807524e5e502cb2f5
Egor Bron <71507444+EgorBron@users.noreply.github.com>
2023-04-18 19:03:18 +0300

    Lua scripting base

---
+++ DESrv.LuaScriptingApi/LuaLoader.cs
@@ -0,0 +1,19 @@
+﻿namespace Blusutils.DESrv.LuaScriptingApi;
+public class LuaLoader {
+
+    string scriptsPath;
+
+    public LuaLoader(string luaScriptsPath) {
+        scriptsPath = luaScriptsPath;
+    }
+
+    public bool Load(string scriptName, string? path = null) {
+        path ??= scriptsPath;
+        return true;
+    }
+
+    public object? DoScript(string scriptName, string? path = null, params object[] args) {
+        Load(scriptName, path);
+        return null;
+    }
+}
